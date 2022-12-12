e5f78551ee0d8e1486acdcd79464721150f86580
Irha Ali <irhaali_97@hotmail.ca>
2022-12-12 15:12:42 -0500

    Event handle ctrl + c, up, down, left, right

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const { connect } = require("./client");
 
+
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -9,12 +10,21 @@ const setupInput = function () {
   return stdin;
 };
 
+// Handles the following user input: ctrl + c, up (w), down (s), left (a), right (d)
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === "w") {
+    conn.write('Move: up');
+  } else if (key === "s") {
+    conn.write('Move: down');
+  } else if (key === "a") {
+    conn.write('Move: left');
+  } else if (key === "d") {
+    conn.write('Move: right');
   }
 };
 
 console.log("Connecting ...");
-connect();
-setupInput();
+let conn = connect();
+setupInput(conn);
