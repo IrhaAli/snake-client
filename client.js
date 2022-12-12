62b74696009bf07e1cb7b468177f23c3e1d51d3d
Irha Ali <irhaali_97@hotmail.ca>
2022-12-12 14:52:50 -0500

    Setup Stdin

--- client.js
+++ client.js
@@ -6,17 +6,16 @@ const connect = function () {
     host: 'localhost',
     port: '50541'
   });
+  // Events as soon as the connection is established
+  conn.on('connect', () => { console.log('Successfully connected to game server'); });
 
-  conn.on('connect', () => { console.log('Successfully connected to game server'); })
+  conn.on("connect", () => { conn.write(`Name: ${Math.random().toString(36).slice(2, 5)}`); });
 
-  conn.on("connect", () => {
-    conn.write(`Name: ${Math.random().toString(36).slice(2, 5)}`);
-  });
 
+  // Events when data is received. This is when you're not moving. It doesn't have anything to do with connect. THIS IS FOR THE CASE OF IDLING
+  conn.on("data", function (data) { console.log(data); });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on("data", function (data) { console.log(data); });
 
   return conn;
 };
